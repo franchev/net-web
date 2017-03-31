8fe1657ece52257a0ddcc5a3506802087d38eabd
Francesca Chevry <franchev@hotmail.com>
2017-03-31 19:19:08 -0400

    After third module on plurasight

---
+++ CitiInfo/CitiInfo.API/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace CitiInfo.API
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddMvcOptions( o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()))
+                    ;
+                // This section is important if we want the user to receive exactly the casing that we used to define these objects
+                // For example, if we named them with upper case, then they'll receive uppercase
+                // otherwise, it'll follow the camel case naming convention when returning json object names
+                //.AddJsonOptions( o =>
+                //{
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                //        castedResolver.NamingStrategy = null;
+                //    }
+                //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+            }
+
+            app.UseStatusCodePages();
+            app.UseMvc();
+
+            //app.Run((context) =>
+            //{
+            //    throw new Exception("Example exception");
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
