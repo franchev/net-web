cf08d78bdb6dc239e3ed41fd8eb3fdc659b9771b
Francesca Chevry <franchev@hotmail.com>
2017-04-02 18:58:12 -0400

    Learning about injection, lots to learn

--- CitiInfo/CitiInfo.API/Startup.cs
+++ CitiInfo/CitiInfo.API/Startup.cs
@@ -9,11 +9,26 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using NLog.Extensions.Logging;
+using CitiInfo.API.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace CitiInfo.API
 {
     public class Startup
     {
+        public static IConfigurationRoot configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appSettings.{env.EnvironmentName}", optional: true, reloadOnChange: true);
+
+            configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -22,23 +37,33 @@ namespace CitiInfo.API
                 .AddMvcOptions( o => o.OutputFormatters.Add(
                     new XmlDataContractSerializerOutputFormatter()))
                     ;
-                // This section is important if we want the user to receive exactly the casing that we used to define these objects
-                // For example, if we named them with upper case, then they'll receive uppercase
-                // otherwise, it'll follow the camel case naming convention when returning json object names
-                //.AddJsonOptions( o =>
-                //{
-                //    if (o.SerializerSettings.ContractResolver != null)
-                //    {
-                //        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
-                //        castedResolver.NamingStrategy = null;
-                //    }
-                //});
+            // This section is important if we want the user to receive exactly the casing that we used to define these objects
+            // For example, if we named them with upper case, then they'll receive uppercase
+            // otherwise, it'll follow the camel case naming convention when returning json object names
+            //.AddJsonOptions( o =>
+            //{
+            //    if (o.SerializerSettings.ContractResolver != null)
+            //    {
+            //        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+            //        castedResolver.NamingStrategy = null;
+            //    }
+            //});
+#if DEBUG
+            services.AddTransient<IMailService, LocalMailService>();
+#else
+            services.AddTransient<IMailService, CloudMailService>();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+
+            loggerFactory.AddDebug();
+
+            //loggerFactory.AddProvider(new NLog.Extensions.Logging.NLogLoggerProvider());
+            loggerFactory.AddNLog();
 
             if (env.IsDevelopment())
             {
